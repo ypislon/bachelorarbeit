f68183d9f4f022fe769bf4734352479277a562a0
ypislon <ypislon@freenet.de>
2018-05-12 02:17:17 +0200

    refactor(spider): fix and remove redundant code

--- article_spider/article_spider/spiders/article.py
+++ article_spider/article_spider/spiders/article.py
@@ -24,52 +24,16 @@ class ArticlesSpider(scrapy.Spider):
         website_name = response.meta["website_name"]
         website = Website.get(Website.name==website_name)
         # get all links to articles
-        for article_links in response.css(website.article_identifier):
+        if "//" in website.article_identifier:
+            r = response.xpath(website.article_identifier)
+        else:
+            r = response.css(website.article_identifier)
+        for r_article in r:
             # create a new article and populate it
             article = Article()
             article.website = website
-            article.url = article_links.css("a::attr(href)").extract_first()
-            article.title = article_links.css("a::text").extract_first()
-
-            # TODO: seed!
-            # article.date_published = response.css(Website.date_identifier + "::text").extract_first()
-
-            # TODO: seed!
-            # author_name = Website.author_identifier + "::text"
-            author_name = "Example Author"
-            author = Author()
-            author.name = author_name
-            article.author = author
-            # Author assignment
-            # if(author_name.contains("," || ";" "|"):
-            #     # do something! you maybe have more than one author
-            # if(Author.get(Author.name==author_name)):
-            #     article.author = Author.get(Author.name==author_name)
-            # else:
-            #     author = Author()
-            #     author.name = author_name
-            #     article.author = author
+            article.url = r_article.css("a::attr(href)").extract_first()
 
             article.save()
-
-            # get all links from the current article
-            # TODO: refactor this to the new spider!
-            # TODO: do we need that additional a?
-            # article_links = response.css(Website.article_identifier).extract()
-            # print(article_links[0])
-            # for article_link in article_links:
-                # link = Link()
-                # a_element = article_link
-                # dafuq? is there a method? maybe XPATH
-                # print(a_element)
-                # link.url = a_element
-                ### parse domain
-                # link.domain = a_element.getDomain()
-                # dafuq? is there a method? maybe XPATH
-                # link.link_text = article_link.getText()
-                # dafuq? is there a method? maybe XPATH
-                # link.article = article
-                # print(link)
-                # link.save()
 
         self.log('Saved article %s' % article.title)
